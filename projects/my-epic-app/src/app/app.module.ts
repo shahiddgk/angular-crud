0e3408695deb0c40cf7392930a7db6be974f2221
shahiddgk <mshahid_ms@yahoo.com>
2020-05-13 22:42:48 +0500

    Create new coupons module

--- projects/my-epic-app/src/app/app.module.ts
+++ projects/my-epic-app/src/app/app.module.ts
@@ -4,14 +4,22 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CouponsComponent } from './coupons/coupons.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CouponsComponent
   ],
   imports: [
     CoreModule,
     AppRoutingModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ projects/my-epic-app/src/app/coupons/coupons.component.ts
@@ -0,0 +1,30 @@
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTable } from '@angular/material/table';
+import { CouponsDataSource, CouponsItem } from './coupons-datasource';
+
+@Component({
+  selector: 'my-org-coupons',
+  templateUrl: './coupons.component.html',
+  styleUrls: ['./coupons.component.scss']
+})
+export class CouponsComponent implements AfterViewInit, OnInit {
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatTable) table: MatTable<CouponsItem>;
+  dataSource: CouponsDataSource;
+
+  /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
+  displayedColumns = ['id', 'name'];
+
+  ngOnInit() {
+    this.dataSource = new CouponsDataSource();
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+    this.table.dataSource = this.dataSource;
+  }
+}
